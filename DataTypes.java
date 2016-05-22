ff1aa114420211a2037a90161e69c07282d5b992
QuintenDavis <Quinten.R.Davis@gmail.com>
2016-05-22 13:18:08 -0400

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+	
+	int sum = 29; //initialization
+	float fl = 3.14f; //declaration
+	float result = sum + fl; 
+	System.out.println(result);
+
+	}
+
+}
